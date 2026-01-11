d7421749fa7aa8dc19dd840734bc3c56d9870a0d
Ao Wei <111731589+ao-wei@users.noreply.github.com>
2026-01-11 14:14:54 +0000

    02_llamaIndex_example.py 改为调用 qwen-plus

--- code/C1/02_llamaIndex_example.py
+++ code/C1/02_llamaIndex_example.py
@@ -4,10 +4,15 @@ from dotenv import load_dotenv
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings 
 from llama_index.llms.deepseek import DeepSeek
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from llama_index.llms.dashscope import DashScope, DashScopeGenerationModels
 
 load_dotenv()
 
-Settings.llm = DeepSeek(model="deepseek-chat", api_key=os.getenv("DEEPSEEK_API_KEY"))
+# Settings.llm = DeepSeek(model="deepseek-chat", api_key=os.getenv("DEEPSEEK_API_KEY"))
+Settings.llm = DashScope(
+    model_name=DashScopeGenerationModels.QWEN_PLUS,
+    api_key=os.getenv("QWEN_API_KEY"),  # 也可以不写，让它自动读环境变量
+)
 Settings.embed_model = HuggingFaceEmbedding("BAAI/bge-small-zh-v1.5")
 
 docs = SimpleDirectoryReader(input_files=["../../data/C1/markdown/easy-rl-chapter1.md"]).load_data()
